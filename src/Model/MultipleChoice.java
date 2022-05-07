adb05e687beffd801aa2c83c533b6b57174b13b3
Maria <mariababko4292@gmail.com>
2022-05-06 19:16:41 -0700

    Stubbed Question and Answer Hierarchy

---
+++ src/Model/MultipleChoice.java
@@ -0,0 +1,4 @@
+package Model;
+
+public class MultipleChoice extends Question{
+}
---
+++ src/Model/Question.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Question {
+    private String question;
+
+    Question() {}
+
+    private class Answer {
+        private String answer;
+    }
+
+    private String askQuestion() {
+        return "";
+    }
+}
