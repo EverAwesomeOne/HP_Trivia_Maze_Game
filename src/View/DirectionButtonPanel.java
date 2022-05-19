e5141afb83a51cdd3a088e2efc37ff076b827ebf
Edwin <solisbrunoedwin@gmail.com>
2022-05-18 20:27:12 -0700

    Implemented UW Java naming conventions for three classes

--- src/View/DirectionButtonPanel.java
+++ src/View/DirectionButtonPanel.java
@@ -8,80 +8,80 @@ import java.awt.*;
 
 public class DirectionButtonPanel extends JPanel {
 
-    private static JPanel directionButtonPanel;
+    private static JPanel myDirectionButtonPanel;
 
-    private final MazePanel MP;
-    private final TriviaMazeBrain triviaMazeBrain;
+    private final MazePanel myMazePanel;
+    private final TriviaMazeBrain myTriviaMazeBrain;
 
     private static final String DIR_NORTH = "NORTH";
     private static final String DIR_SOUTH = "SOUTH";
     private static final String DIR_WEST = "WEST";
     private static final String DIR_EAST = "EAST";
 
-    BasicArrowButton northButton;
-    BasicArrowButton southButton;
-    BasicArrowButton eastButton;
-    BasicArrowButton westButton;
+    private final BasicArrowButton myNorthButton;
+    private final BasicArrowButton mySouthButton;
+    private final BasicArrowButton myEastButton;
+    private final BasicArrowButton myWestButton;
 
-    DirectionButtonPanel(JPanel gamePanel, MazePanel MP, TriviaMazeBrain triviaMazeBrain) {
-        this.MP = MP;
-        this.triviaMazeBrain = triviaMazeBrain;
-        directionButtonPanel = new JPanel();
-        directionButtonPanel.setLayout(new GridLayout(3,3));
-        directionButtonPanel.setBorder(BorderFactory.createTitledBorder("Choose Door"));
+    DirectionButtonPanel(final JPanel theGamePanel, final MazePanel theMazePanel, final TriviaMazeBrain theTriviaMazeBrain) {
+        myMazePanel = theMazePanel;
+        myTriviaMazeBrain = theTriviaMazeBrain;
+        myDirectionButtonPanel = new JPanel();
+        myDirectionButtonPanel.setLayout(new GridLayout(3,3));
+        myDirectionButtonPanel.setBorder(BorderFactory.createTitledBorder("Choose Door"));
 
-        northButton = new BasicArrowButton(BasicArrowButton.NORTH);
-        southButton = new BasicArrowButton(BasicArrowButton.SOUTH);
-        eastButton = new BasicArrowButton(BasicArrowButton.EAST);
-        westButton = new BasicArrowButton(BasicArrowButton.WEST);
+        myNorthButton = new BasicArrowButton(BasicArrowButton.NORTH);
+        mySouthButton = new BasicArrowButton(BasicArrowButton.SOUTH);
+        myEastButton = new BasicArrowButton(BasicArrowButton.EAST);
+        myWestButton = new BasicArrowButton(BasicArrowButton.WEST);
 
         // Replace with an icon?
-        JLabel centerLabel = new JLabel("<html>Current<br/>Room</html>", SwingConstants.CENTER);
+        final JLabel centerLabel = new JLabel("<html>Current<br/>Room</html>", SwingConstants.CENTER);
 
         // initialize BasicArrowButtons based on the start position in maze
         disableButtons();
 
-        addArrowActionListener(northButton, DIR_NORTH);
-        addArrowActionListener(southButton, DIR_SOUTH);
-        addArrowActionListener(westButton, DIR_WEST);
-        addArrowActionListener(eastButton, DIR_EAST);
+        addArrowActionListener(myNorthButton, DIR_NORTH);
+        addArrowActionListener(mySouthButton, DIR_SOUTH);
+        addArrowActionListener(myWestButton, DIR_WEST);
+        addArrowActionListener(myEastButton, DIR_EAST);
 
         // empty JLabels act as placeholders in GridLayout
         // to distance BasicArrowButtons
-        directionButtonPanel.add(new JLabel(""));
-        directionButtonPanel.add(northButton);
-        directionButtonPanel.add(new JLabel(""));
-        directionButtonPanel.add(westButton);
-        directionButtonPanel.add(centerLabel);
-        directionButtonPanel.add(eastButton);
-        directionButtonPanel.add(new JLabel(""));
-        directionButtonPanel.add(southButton);
-        directionButtonPanel.add(new JLabel(""));
+        myDirectionButtonPanel.add(new JLabel(""));
+        myDirectionButtonPanel.add(myNorthButton);
+        myDirectionButtonPanel.add(new JLabel(""));
+        myDirectionButtonPanel.add(myWestButton);
+        myDirectionButtonPanel.add(centerLabel);
+        myDirectionButtonPanel.add(myEastButton);
+        myDirectionButtonPanel.add(new JLabel(""));
+        myDirectionButtonPanel.add(mySouthButton);
+        myDirectionButtonPanel.add(new JLabel(""));
 
-        gamePanel.add(directionButtonPanel);
+        theGamePanel.add(myDirectionButtonPanel);
 
-        directionButtonPanel.setVisible(true);
+        myDirectionButtonPanel.setVisible(true);
     }
 
-    private void addArrowActionListener(BasicArrowButton arrowButton, String arrowDirection) {
-        arrowButton.addActionListener(
+    private void addArrowActionListener(final BasicArrowButton theArrowButton, final String theArrowDirection) {
+        theArrowButton.addActionListener(
                 e -> {
-                    triviaMazeBrain.move(arrowDirection);
+                    myTriviaMazeBrain.move(theArrowDirection);
                 }
         );
     }
 
     void disableButtons() {
-        northButton.setEnabled(MP.validDirection(DIR_NORTH));
-        southButton.setEnabled(MP.validDirection(DIR_SOUTH));
-        westButton.setEnabled(MP.validDirection(DIR_WEST));
-        eastButton.setEnabled(MP.validDirection(DIR_EAST));
+        myNorthButton.setEnabled(myMazePanel.validDirection(DIR_NORTH));
+        mySouthButton.setEnabled(myMazePanel.validDirection(DIR_SOUTH));
+        myWestButton.setEnabled(myMazePanel.validDirection(DIR_WEST));
+        myEastButton.setEnabled(myMazePanel.validDirection(DIR_EAST));
     }
 
     void disableAllButtons() {
-        northButton.setEnabled(false);
-        southButton.setEnabled(false);
-        westButton.setEnabled(false);
-        eastButton.setEnabled(false);
+        myNorthButton.setEnabled(false);
+        mySouthButton.setEnabled(false);
+        myWestButton.setEnabled(false);
+        myEastButton.setEnabled(false);
     }
 }
