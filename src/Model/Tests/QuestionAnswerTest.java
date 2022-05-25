08d492055d49dc9441ac1f18af789e27f0075859
Chloe D <owlgirlpnw@gmail.com>
2022-05-25 15:25:18 -0700

    Stubbed QuestionAnswer unit tests

--- src/Model/Tests/QuestionAnswerTest.java
+++ src/Model/Tests/QuestionAnswerTest.java
@@ -1,4 +1,26 @@
 package Model.Tests;
 
+import org.junit.Test;
+
 public class QuestionAnswerTest {
+
+    @Test
+    public void testQuestionAnswerConstructor() {
+
+    }
+
+    @Test
+    public void testQuestionAnswerFromDatabase() {
+
+    }
+
+    @Test
+    public void testGetQuestionList() {
+
+    }
+
+    @Test
+    public void testSelectedCorrectAnswer() {
+
+    }
 }
