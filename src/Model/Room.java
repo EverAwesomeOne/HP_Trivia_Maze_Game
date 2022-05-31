5b7406cf60abcd02c20ae5350c9de24ef3e0150a
Maria <mariababko4292@gmail.com>
2022-05-31 11:55:54 -0700

    Added Java Docs to Direction, Door, Room, and QuestionAnswer

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -3,6 +3,9 @@ package Model;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * The Room class represents a specific room in the maze.
+ */
 public class Room implements Serializable {
     private Door myNorthDoor;
     private Door myEastDoor;
@@ -12,6 +15,10 @@ public class Room implements Serializable {
     @Serial
     private static final long serialVersionUID = 109174852462682090L;
 
+    /**
+     * Constructor for the Room class.
+     * Initializes all four doors that are in the room.
+     */
     Room() {
         myNorthDoor = new Door();
         myEastDoor = new Door();
@@ -19,6 +26,11 @@ public class Room implements Serializable {
         myWestDoor = new Door();
     }
 
+    /**
+     * Gets the door specified from the direction being given.
+     * @param theDirection - the direction of the door needed
+     * @return - the door located in that specific direction
+     */
     public Door getDoor(final Direction theDirection) {
         if (theDirection == Direction.NORTH) {
             return myNorthDoor;
@@ -31,6 +43,13 @@ public class Room implements Serializable {
         }
     }
 
+    /**
+     * Sets a door to have the same reference as another door depending on the
+     * direction to make it easy to update when a player goes through a door
+     * and into another room.
+     * @param theDirection - the direction in which the other door is located
+     * @param theDoorToShare - the door that will share the reference
+     */
     void setSharedDoor(final Direction theDirection, final Door theDoorToShare) {
         if (theDirection == Direction.NORTH) {
             myNorthDoor = theDoorToShare;
