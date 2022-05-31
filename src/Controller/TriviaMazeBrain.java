927d5a4150e1cf51d22ce44a2adb7b48bd98fcb0
Maria <mariababko4292@gmail.com>
2022-05-31 12:38:51 -0700

    Added Java Docs to TriviaMazeBrain

--- src/Controller/TriviaMazeBrain.java
+++ src/Controller/TriviaMazeBrain.java
@@ -12,6 +12,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * The TriviaMazeBrain class represents the Controller that acts as the connector between
+ * the View with the GUI and the Model with the logic. It helps to start the game and passes
+ * information between the View and the Model.
+ */
 public class TriviaMazeBrain {
     private Connection myConnection;
     private Statement myStatement;
@@ -20,7 +25,7 @@ public class TriviaMazeBrain {
     private MainFrame myMainFrame;
     private MazePanel myMazePanel;
 
-    private String triviaMazeFile = "triviaMaze.txt";
+    private final String triviaMazeFile = "triviaMaze.txt";
 
     public final static int MAZE_LENGTH = 7;
     public final static String MOVE_FREELY = "Move freely";
@@ -29,16 +34,31 @@ public class TriviaMazeBrain {
         new TriviaMazeBrain();
     }
 
+    /**
+     * Constructor for the TriviaMazeBrain class.
+     * Starts running the game.
+     */
     public TriviaMazeBrain() {
         runGame();
     }
 
+    /**
+     * Sets up the needed pieces of the Model, the database,
+     * and the View to be able to run the game.
+     */
     private void runGame() {
         myMaze = new Maze(MAZE_LENGTH);
         openDatabaseConnection();
         myMainFrame = new MainFrame(this);
     }
 
+    /**
+     * Generates and gets the question and answers for the GUI to set up
+     * and have the player see.
+     * If the player has already answered the question correctly at this door,
+     * the player can just move freely through the door.
+     * @param theDirectionType - the direction in which the door is located
+     */
     public void setUpQuestion(final String theDirectionType) {
         myGamePanel = myMainFrame.getGamePanel();
         myMazePanel = myGamePanel.getMazePanel();
@@ -54,6 +74,14 @@ public class TriviaMazeBrain {
         }
     }
 
+    /**
+     * Move the player by updating where they are in the maze from the Model side
+     * as well as the View side.
+     * If the player has already answered a question correctly at this door,
+     * they can just move freely through the door.
+     * @param theUserAnswer - the player answer
+     * @param theDirectionType - the direction in which the door is located
+     */
     public void moveCharacter(final String theUserAnswer, final String theDirectionType) {
 
         final Direction directionToMove = Direction.valueOf(theDirectionType);
@@ -74,6 +102,14 @@ public class TriviaMazeBrain {
                 myMaze.getCharacterColumn());
     }
 
+    /**
+     * Updates the maze in Model and the GUI parts in View to reflect that the player
+     * has gotten the answer correct.
+     * @param theUserAnswer - the player answer
+     * @param theDirectionType - the String version of the direction in which the door is located
+     * @param theDirectionToMove - the Enum version of the direction in which the door is located
+     * @param theQA - the question and answers attached to the chosen door
+     */
     private void answeredCorrectly(final String theUserAnswer, final String theDirectionType,
                                    final Direction theDirectionToMove, final QuestionAnswer theQA) {
         myMazePanel.setDoorIcon(myMaze.getCharacterRow(), myMaze.getCharacterColumn(),
@@ -86,6 +122,16 @@ public class TriviaMazeBrain {
         }
     }
 
+    /**
+     * Updates the maze in Model and the GUI parts in View to reflect that the player
+     * has gotten the answer wrong.
+     * If there are no paths left to get to the exit, the player will be displayed
+     * a game over message.
+     * @param theUserAnswer - the player answer
+     * @param theDirectionType - the direction in which the door is located
+     * @param theChosenDoor - the door that was picked to go through by the player
+     * @param theQA - the question and answers attached to the chosen door
+     */
     private void answeredIncorrectly(final String theUserAnswer, final String theDirectionType,
                                      final Door theChosenDoor, final QuestionAnswer theQA) {
         theChosenDoor.lockDoor();
@@ -99,28 +145,56 @@ public class TriviaMazeBrain {
         }
     }
 
+    /**
+     * Checks to see if a specific door in the current room is locked or not.
+     * @param theDirectionType - the direction in which the door is located
+     * @return - if the door is locked or not
+     */
     public boolean checkCurrentRoomIsLockedStatus(final String theDirectionType) {
         return !myMaze.getCurrentRoom().getDoor
                 (Direction.valueOf(theDirectionType)).isLocked();
     }
 
+    /**
+     * Checks to see if the door is locked or not.
+     * @param theDirectionType - the direction in which the door is located
+     * @param theRow - the current row of the player
+     * @param theCol - the current column of the player
+     * @return - if the door is locked or not
+     */
     public boolean checkSpecificRoomIsNotLocked(final String theDirectionType,
                                                 final int theRow, final int theCol) {
         return myMaze.getSpecificRoom(theRow, theCol).getDoor(
                 Direction.valueOf(theDirectionType)).isLocked();
     }
 
+    /**
+     * Checks to see if the player has been to this door or not.
+     * @param theDirectionType - the direction in which the door is located
+     * @param theRow - the current row of the player
+     * @param theCol - the current column of the player
+     * @return - if the player has been to this door or not
+     */
     public boolean checkDoorIsNotFirstTime(final String theDirectionType,
                                            final int theRow, final int theCol) {
         return !myMaze.getSpecificRoom(theRow, theCol).getDoor(
                 Direction.valueOf(theDirectionType)).wasFirstTime();
     }
 
+    /**
+     * Gets rid of all the current game related parts and
+     * reruns the game.
+     */
     public void resetGameState() {
         myMainFrame.dispose();
         runGame();
     }
 
+    /**
+     * Serializes the information and stores it in a file to
+     * remember what state the game was in when the player
+     * saved the game.
+     */
     public void serialize() {
 
         try {
@@ -140,6 +214,11 @@ public class TriviaMazeBrain {
         }
     }
 
+    /**
+     * Deserializes the information stored in a file to
+     * remember what state the game was in when the player
+     * saved the game.
+     */
     public void deserialize() {
         try {
             FileInputStream file = new FileInputStream
@@ -160,15 +239,25 @@ public class TriviaMazeBrain {
         }
     }
 
+    /**
+     * Gets the row of where the character/player is located in the maze.
+     * @return - the row of where the character/player is located in the maze
+     */
     public int getRow() {
         return myMaze.getCharacterRow();
     }
 
+    /**
+     * Gets the column of where the character/player is located in the maze.
+     * @return - the column of where the character/player is located in the maze
+     */
     public int getColumn() {
         return myMaze.getCharacterColumn();
     }
 
-
+    /**
+     * Opens the connection to the "questions" database.
+     */
     public void openDatabaseConnection() {
         SQLiteDataSource ds = null;
 
@@ -190,6 +279,9 @@ public class TriviaMazeBrain {
 
     }
 
+    /**
+     * Closes the connection to the "questions" database.
+     */
     public void closeDatabaseConnection() {
         if (myStatement != null) {
             try {
